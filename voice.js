e3deeb92fd93cf9c3464f121dab38850c6670eb4
s9pimenn <s9pimenn@stud.uni-saarland.de>
2012-06-25 22:36:08 +0200

    Improved Dialogue Manager -screen locks during audio output -alternative input (text) is provided after 3 failures in speech input for the same object

--- voice.js
+++ voice.js
@@ -7,11 +7,12 @@
 
 //this is the div for googleVoice
 var gvContainer = document.createElement('div');
-gvContainer.id = "gvContainer";
+gvContainer.setAttribute('id', 'gvContainer');
 gvContainer.setAttribute('class', 'popup');
 var googleVoice = document.createElement('input');
-googleVoice.id = "googleVoice";
-googleVoice.type = "text";
+googleVoice.setAttribute('id', 'googleVoice');
+googleVoice.setAttribute('class', 'popup');
+googleVoice.setAttribute('type', 'text');
 googleVoice.onwebkitspeechchange = function() {
     onChange();
 };
@@ -21,11 +22,11 @@ googleVoice.onwebkitspeecherror = function() {
 };
 googleVoice.setAttribute('x-webkit-speech', 'x-webkit-speech');
 var closeButton = document.createElement('button');
-closeButton.id = 'closeButton';
+closeButton.setAttribute('id', 'closeButton');
 var buttonText = document.createTextNode('Close');
 closeButton.appendChild(buttonText);
 closeButton.onclick = function() {
-    makeInvisible();
+    makeInputInvisible(googleVoice, gvContainer);
     clickCounter = 0;
 };
 gvContainer.appendChild(closeButton);
@@ -35,13 +36,15 @@ document.body.appendChild(gvContainer);
 //this appends the Spoken Interaction Functionaloty to all img elements in the page
 var images = document.getElementsByTagName('img');
 for(var i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", makeVisible, false);
+    images[i].addEventListener("click", function() {
+        makeInputVisible(googleVoice, gvContainer)
+    }, false);
 };
 
 //this should lock the screen during spoken output
-var lockScreen = document.createElement('div');
-lockScreen.id = 'lockScreen';
-document.body.appendChild(lockScreen);
+var lockingScreen = document.createElement('div');
+lockingScreen.setAttribute('id', 'lockScreen');
+document.body.appendChild(lockingScreen);
 
 //this is the Spoken Output element
 var spokenOutput = document.createElement('audio');
@@ -53,28 +56,51 @@ var prevClickedElement;
 var clickedElement;
 var clickCounter = 0;
 
-function makeVisible() {
-    googleVoice.value = '';
-    gvContainer.style.visibility = 'visible';
+//this is the alternative input element
+var altInputContainer = document.createElement('div');
+altInputContainer.setAttribute('id', 'altInputContainer');
+altInputContainer.setAttribute('class', 'popup');
+var altInput = document.createElement('input');
+altInput.setAttribute('class', 'popup');
+altInput.setAttribute('id', 'altInput');
+altInput.setAttribute('type', 'text');
+var altInputSubmit = document.createElement('input');
+altInputSubmit.setAttribute('id', 'altInputSubmit');
+altInputSubmit.setAttribute('type', 'submit');
+altInputSubmit.setAttribute('value', 'Tag it!');
+altInputSubmit.onclick = function() {
+    var val = altInput.value
+    val.toLowerCase();
+    var results = runParser(val.split(/\s+/));
+    dialogueManager(results, val);
+    makeInputInvisible(altInput, altInputContainer);
+};
+altInputContainer.appendChild(altInput);
+altInputContainer.appendChild(altInputSubmit);
+document.body.appendChild(altInputContainer);
+
+function makeInputVisible(inputElement, containerElement) {
+    inputElement.value = '';
+    containerElement.style.visibility = 'visible';
 }
 
-function makeInvisible() {
-    gvContainer.style.visibility = 'hidden';
-    googleVoice.value = '';
+function makeInputInvisible(inputElement, containerElement) {
+    containerElement.style.visibility = 'hidden';
+    inputElement.value = '';
 }
 
 function onChange() {
     //this function processes what the user said with js-chartparser
-    if(typeof(prevClickedElement) !== 'undefined'){
+    if( typeof (prevClickedElement) !== 'undefined') {
         prevClickedElement = clickedElement;
         clickedElement = event.target;
-    }else{
+    } else {
         prevClickedElement = event.target;
     }
-    
+
     var val = event.target.value;
     alert("you said:" + val);
-    makeInvisible();
+    makeInputInvisible(googleVoice, gvContainer);
     val.toLowerCase();
     results = runParser(val.split(/\s+/));
     dialogueManager(results, val);
@@ -83,24 +109,28 @@ function onChange() {
 function dialogueManager(results, val) {
     //this function imitates a dialogue manager and handles diverse errors or unparseable user input
     if(parseInt(results[0]) == 0) {
-        
-        if(clickedElement == prevClickedElement && clickCounter >= 3){
+
+        if(clickedElement == prevClickedElement && clickCounter >= 3) {
+            makeInputInvisible(googleVoice, gvContainer);
             alert('This was wrong too many times');
+            makeInputVisible(altInput, altInputContainer);
+            clickCounter = 0;
         } else {
-        clickCounter += 1;
-        clickedElement = prevClickedElement;
-        speekOutput(val, 'unparseable');
-        makeVisible();
+            clickCounter += 1;
+            clickedElement = prevClickedElement;
+            speekOutput(val, 'unparseable');
+            makeInputVisible(googleVoice, gvContainer);
         }
-        
+
     }
 
     if(parseInt(results[0]) >= 1) {
         n = parseInt(results[0])
-        for(var i = 0; i < n; i++) {
-            alert("result "+i+": "+results[i]);
+        for(var i = 1; i < n; i++) {
+            alert("result " + i + ": " + results[i]);
         };
         clickCounter = 0;
+
     }
 }
 
@@ -120,7 +150,7 @@ function speekOutput(customMessage, status) {
             str += messageArray[i] + "+";
         }
     };
- 
+
     var output = "";
 
     if(status == 'unparseable') {
@@ -129,7 +159,7 @@ function speekOutput(customMessage, status) {
     if(status == 'error') {
         output += str;
     }
-    
+
     var maryString = "process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT=" + output + "&OUTPUT_TEXT=&effect_Volume_selected=&effect_Volume_parameters=amount%3A2.0%3B&effect_Volume_default=Default&effect_Volume_help=Help&effect_TractScaler_selected=&effect_TractScaler_parameters=amount%3A1.5%3B&effect_TractScaler_default=Default&effect_TractScaler_help=Help&effect_F0Scale_selected=&effect_F0Scale_parameters=f0Scale%3A2.0%3B&effect_F0Scale_default=Default&effect_F0Scale_help=Help&effect_F0Add_selected=&effect_F0Add_parameters=f0Add%3A50.0%3B&effect_F0Add_default=Default&effect_F0Add_help=Help&effect_Rate_selected=&effect_Rate_parameters=durScale%3A1.5%3B&effect_Rate_default=Default&effect_Rate_help=Help&effect_Robot_selected=&effect_Robot_parameters=amount%3A100.0%3B&effect_Robot_default=Default&effect_Robot_help=Help&effect_Whisper_selected=&effect_Whisper_parameters=amount%3A100.0%3B&effect_Whisper_default=Default&effect_Whisper_help=Help&effect_Stadium_selected=&effect_Stadium_parameters=amount%3A100.0&effect_Stadium_default=Default&effect_Stadium_help=Help&effect_Chorus_selected=&effect_Chorus_parameters=delay1%3A466%3Bamp1%3A0.54%3Bdelay2%3A600%3Bamp2%3A-0.10%3Bdelay3%3A250%3Bamp3%3A0.30&effect_Chorus_default=Default&effect_Chorus_help=Help&effect_FIRFilter_selected=&effect_FIRFilter_parameters=type%3A3%3Bfc1%3A500.0%3Bfc2%3A2000.0&effect_FIRFilter_default=Default&effect_FIRFilter_help=Help&effect_JetPilot_selected=&effect_JetPilot_parameters=&effect_JetPilot_default=Default&effect_JetPilot_help=Help&HELP_TEXT=&exampleTexts=I'm%20Spike.&VOICE_SELECTIONS=dfki-spike%20en_GB%20male%20unitselection%20general&AUDIO_OUT=WAVE_FILE&LOCALE=en_GB&VOICE=dfki-spike&AUDIO=WAVE_FILE";
 
     $.ajax({
@@ -138,11 +168,19 @@ function speekOutput(customMessage, status) {
         dataType : 'jsonp audio/x-wav',
         statusCode : {
             404 : function() {
-                alert("I am sorry, I did not understand you! Please rephrase your command!");
+                if(status == 'unparseable') {
+                    alert('I am sorry, I could not understand what "' + customMessage + '" is supposed to mean! Please rephrase your command!');
+                }
+                if(status == 'error') {
+                    alert('We are sorry, an unexpected error has occured!');
+                }
             },
             200 : function() {
+                spokenOutput.load();
+                lockScreen();
                 spokenOutput.setAttribute('src', url + maryString);
                 spokenOutput.play();
+                spokenOutput.addEventListener('ended',unlockScreen,false);
             }
         },
         cache : false,
@@ -150,6 +188,10 @@ function speekOutput(customMessage, status) {
 }
 
 function lockScreen() {
-    alert('screen will be locked');
-    lockScreen.style.visibility = 'visible';
+    $("input").blur();
+    lockingScreen.style.visibility = 'visible';
+}
+
+function unlockScreen() {
+    lockingScreen.style.visibility = 'hidden';
 }
