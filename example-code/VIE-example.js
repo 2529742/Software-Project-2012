103366404db80299791712231fc04848dc56f44b
donydwk <donydwk@gmail.com>
2012-07-04 17:59:38 +0200

    examplecode
    Upload the runnable code for example of stanbol analyze

---
+++ example-code/VIE-example.js
@@ -0,0 +1,13 @@
+$(function() {
+  var vie = new VIE();
+  vie.use(new vie.StanbolService(), "stanbol");
+  vie.analyze({
+              element: $('#test')
+              }).using("stanbol").done(function(ent) {
+                                       _.each(ent, function(e) {
+                                              if (e.isof("dbpedia:Person")) {
+                                              $('#result').append($('<p>').text(e.id));
+                                              }
+                                              });
+                                       }).execute();
+  })
